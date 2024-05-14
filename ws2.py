4fb95e59ecbf6f3376710299aef77e56e38afa29
Zeekyy <tom.agrpro@gmail.com>
2024-05-14 10:40:53 +0000

    Add exo2

---
+++ ws2.py
@@ -0,0 +1,5 @@
+# Rédigez un programme ws3.py recherchant un entier selon la méthode dichotomique.
+# Commencez par trier le tableau en entrée par ordre croissant.
+# La recherche dichotomique consiste à vérifier l’item au milieu du tableau, et selon la valeur, 
+# d’aller chercher une valeur vers le quart suivant du tableau, jusqu’à trouver (ou non) la 
+# valeur souhaitée.
