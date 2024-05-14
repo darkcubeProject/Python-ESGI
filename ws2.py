d933d894f598c2497e8150220f53367637ca7679
NoaMatout <165659748+NoaMatout@users.noreply.github.com>
2024-05-14 15:41:23 +0200

    jknjk

--- ws2.py
+++ ws2.py
@@ -3,3 +3,26 @@
 # La recherche dichotomique consiste à vérifier l’item au milieu du tableau, et selon la valeur, 
 # d’aller chercher une valeur vers le quart suivant du tableau, jusqu’à trouver (ou non) la 
 # valeur souhaitée.
+
+data=[78, 30, 9, 10, 15]
+data.sort()
+print (data)
+search = 9
+start = 0
+end = len(data) - 1
+
+while True:
+    middle = (start + end) // 2
+    print (middle)
+    n = data [middle]
+    if n > search:
+        end = middle - 1
+    elif n < search:
+        start = middle + 1
+    else:
+        print("la veleur est" + str(middle))
+        break
+    if start >= end:
+        print("pas de result")
+        break
+print(data)
