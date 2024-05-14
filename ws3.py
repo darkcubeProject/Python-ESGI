78c9f907d94137861ac66fe3e9081a0d63f372f3
Zeekyy <tom.agrpro@gmail.com>
2024-05-14 15:05:29 +0000

    ajout fonction flatten et expand

--- ws3.py
+++ ws3.py
@@ -11,3 +11,12 @@ def gen_pos(n=5):
             items[x] = 1
             found += 1
     return items
+
+def flatten (row,col,n=5):
+    return row * n + col
+
+def expand (x,n=5):
+    return(x//n,x%n)
+
+position_dame = ['2','8','11','16','23']
+
