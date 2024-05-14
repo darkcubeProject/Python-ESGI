a7752a5098ab3a3e6d2b249adab7a6516d447202
Zeekyy <tom.agrpro@gmail.com>
2024-05-14 12:08:52 +0000

    tableau dans l'odre croissant

--- ws2.py
+++ ws2.py
@@ -3,3 +3,9 @@
 # La recherche dichotomique consiste à vérifier l’item au milieu du tableau, et selon la valeur, 
 # d’aller chercher une valeur vers le quart suivant du tableau, jusqu’à trouver (ou non) la 
 # valeur souhaitée.
+
+from tabulate import tabulate
+
+tableau = ['3','2','1','4','6','8','7','5','0','9']
+tableau.sort()
+print(tabulate(tableau, headers='firstrow', tablefmt='fancy_grid'))
