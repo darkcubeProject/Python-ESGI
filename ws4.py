9022e4d502f93e635900243f74300daac6f987fd
Zeekyy <tom.agrpro@gmail.com>
2024-05-15 09:44:24 +0000

    test

---
+++ ws4.py
@@ -0,0 +1,13 @@
+import math
+from collections import Counter
+
+text = text.lower()
+text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum non mauris in tempor. Phasellus fringilla varius elit vel mattis. Proin cursus malesuada urna a viverra. Cras fermentum porta efficitur. Sed tincidunt accumsan lacinia. Sed auctor congue finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec scelerisque est vitae congue ornare."
+
+l1 = "e"
+l2 = "i"
+l3 = "d"
+l4 = "a"
+l5 = "c"
+
+for 
