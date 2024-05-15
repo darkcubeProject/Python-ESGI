5edacc9022691c7197838430ae3d0c31e5a39794
Zeekyy <tom.agrpro@gmail.com>
2024-05-15 10:06:20 +0000

    réglage de la barre qui voulait pas augmenter en fonction du nombre de lettre

--- ws4.py
+++ ws4.py
@@ -3,7 +3,6 @@
 # largeur de 40 caractères de large. La largeur de la barre doit être proportionnelle à la
 # fréquence.
 
-import math
 from collections import Counter
 
 text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum non mauris in tempor. Phasellus fringilla varius elit vel mattis. Proin cursus malesuada urna a viverra. Cras fermentum porta efficitur. Sed tincidunt accumsan lacinia. Sed auctor congue finibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec scelerisque est vitae congue ornare."
@@ -11,10 +10,16 @@ text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum no
 def analyse_text(text):
 
     lettres= 'ceida'
+    text = text.lower()
     compteur = Counter(c for c in text if c in lettres)
 
+    max_freq = max(compteur.values(), default=1)
+    largeur_max = 40
+
+
     for lettre in lettres:
-        freq = compteur[lettres]
-        print(f"{lettre}: {'■'} ({freq})")
+        freq = compteur.get(lettre, 0)
+        largeur_barre = int((freq / max_freq) * largeur_max)
+        print(f"{lettre}: {'■' * largeur_barre} ({freq})")
 
 analyse_text(text)
