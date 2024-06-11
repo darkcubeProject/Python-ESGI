d99d114208d36182ac4adf932cf1cdfa78a694e0
NoaMatout <165659748+NoaMatout@users.noreply.github.com>
2024-06-11 21:40:29 +0200

    Update ws6.py

--- ws6.py
+++ ws6.py
@@ -1,2 +1,6 @@
 import threading
 import random
+
+def generer_liste_aleatoire(bas, haut, taille):
+    """Générer une liste de nombres aléatoires."""
+    return [random.randint(bas, haut) for _ in range(taille)]
